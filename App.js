865b95c457659350279f497edd617a5c25a0579b
z7rmc9dfyc-cmd <z7rmc9dfyc@privaterelay.appleid.com>
2026-06-07 21:11:49 -0400

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,4 @@
-<script src="App.js"></script>
+// Test ultra simple
+const root = document.getElementById('root');
+root.innerHTML = "<h1>Le système fonctionne !</h1>";
+console.log("Test réussi");
