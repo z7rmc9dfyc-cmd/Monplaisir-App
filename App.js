54ff697bdf0fe5a97058d01cfaf8f54cb656306c
z7rmc9dfyc-cmd <z7rmc9dfyc@privaterelay.appleid.com>
2026-06-11 08:06:13 -0400

    Add afficher function to update main container

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
-
+function afficher(section) {
+    const conteneur = document.getElementById('conteneur-principal');
+    conteneur.innerHTML = `
+        <h1>${section.toUpperCase()}</h1>
+        <p>Tu es bien dans la section ${section}.</p>
+        <button onclick="location.reload()">Retour Accueil</button>
+    `;
+}
