a1d5e8af59925c336f729530b8561b3c7e6b1e06
z7rmc9dfyc-cmd <z7rmc9dfyc@privaterelay.appleid.com>
2026-06-11 07:59:45 -0400

    Remove console logs and afficher function

--- App.js
+++ App.js
@@ -1,7 +1,1 @@
-console.log("App.js est bien chargé");
 
-function afficher(section) {
-    console.log("Clic détecté sur : " + section);
-    const conteneur = document.getElementById('conteneur-principal');
-    conteneur.innerHTML = "<h2>Section : " + section + "</h2>";
-}
